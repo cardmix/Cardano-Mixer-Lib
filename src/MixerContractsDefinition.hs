6d71dd8f9d69cbe84180cb1f6b84aee1e265fdc5
Cardano Mixer <91214090+cardmix@users.noreply.github.com>
2022-01-23 21:19:08 +0300

    Add contract definitions

---
+++ src/MixerContractsDefinition.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE NamedFieldPuns     #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RankNTypes         #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeFamilies       #-}
+{-# LANGUAGE TypeOperators      #-}
+
+
+module MixerContractsDefinition where
+
+import           Data.Aeson                          (FromJSON(..), ToJSON(..))
+import qualified Data.OpenApi
+import           GHC.Generics                        (Generic)
+import           Prelude                             (Eq, Ord, Show)
+
+data MixerContractsDefinition = MintAdminKey | UseMixer | QueryMixer | RetrieveTimeLocked
+    deriving (Eq, Ord, Show, Generic, FromJSON, ToJSON)
+    deriving anyclass (Data.OpenApi.ToSchema)
+
